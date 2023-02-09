1d582dd298c9b9976ef2d7e138f0f2aa5cfb1d31
AmrGadalla <Gadalla97@hotmail.com>
2023-02-10 01:35:42 +0400

    Modifications done to testPassword Page
    First install - npm i zxcvbn-async@0.0.5
    
    this library is for calculating the duration of how long it will take to crack the password
    
    in the test password page we added the conditions for the different level of passwords

--- src/components/StrengthMeter/index.js
+++ src/components/StrengthMeter/index.js
@@ -1,67 +1,104 @@
-import React from "react";
+import React, {useState} from "react";
+import zxcvbnAsync from "zxcvbn-async";
 import "./style.css";
-const StrengthMeter = (props) => {
-  const pwdValidate = props.password;
-  const initPwdChecker = () => {
-    let pwdCheck = 0;
-    let validateRegex = ["[A-Z]", "[a-z]", "[0-9]", "\\W"];
-    validateRegex.forEach((regex, i) => {
-      if (new RegExp(regex).test(pwdValidate)) {
-        pwdCheck += 1;
-      }
-    });
-    switch (pwdCheck) {
-      case 0:
-        return {
-          strength: 0,
-          val: "",
-        };
-      case 1:
-        return {
-          strength: 1,
-          val: "weak",
-        };
-      case 2:
-        return {
-          strength: 2,
-          val: "fair",
-        };
-      case 3:
-        return {
-          strength: 3,
-          val: "good",
-        };
-      case 4:
-        return {
-          strength: 4,
-          val: "strong",
-        };
-      default:
-        return null;
-    }
-  };
+const StrengthMeter = () => {
+  	const [password, setPassword] = useState('');
+  	const [crackDuration, setCrackDuration] = useState(null);
+	const [passwordStrength, setPasswordStrength] = useState([]);
 
-  return (
-    <>
-      <div className="wrapper">
-        <progress
-          className={`pwd-checker-bar strength-${initPwdChecker().val}`}
-          value={initPwdChecker().strength}
-          max="4"
-        />
-        <br />
-        <p className="pwd-label">
-          {props.password && (
-            <div>
-              <p className={`label strength-${initPwdChecker().val}`}>
-                Password strength validation:
-                <strong>{initPwdChecker().val} </strong>
-              </p>
-            </div>
-          )}
-        </p>
-      </div>
-    </>
-  );
+	const handleChange = (event) => {
+		setPassword(event.target.value);
+		zxcvbnAsync.load()
+		.then((zxcvbn) => {
+			var results = zxcvbn(event.target.value);
+			setCrackDuration(results.crack_times_display.offline_slow_hashing_1e4_per_second)
+		});
+		setPasswordStrength(passwordChecker(event.target.value));
+	};
+
+	const passwordChecker = (password) => {
+		if(password.length >= 15 && /[A-Za-z0-9][!@#$%^&*()_+\-=[\]{};':"|,.<>/?]/.test(password)){
+			return {
+				strength: 4,
+				val: "strong",
+				backgroundColor: '#01a917'
+			}
+		}
+		if(password.length >= 10 && /[A-Za-z0-9][!@#$%^&*_+\-=;':"|,.<>/?]/.test(password)){
+			return {
+				strength: 3,
+				val: "good",
+				backgroundColor: '#0b75ed'
+			}
+		}
+		if(password.length >= 8 && /[A-Za-z0-9]/.test(password)){
+			return {
+				strength: 2,
+				val: "fair",
+				backgroundColor: '#ebbd04'
+			}
+		}
+		if(password.length <= 7 && /[A-Za-z]/.test(password)){
+			return {
+				strength: 1,
+				val: "weak",
+				backgroundColor: '#e20b07'
+			}
+		}
+
+		return {
+			strength: 0,
+			val: "",
+			backgroundColor: ''
+        }
+	}
+
+  	return (
+		<div className='w-100 d-flex flex-column justify-content-center'>
+			<div className='row'>
+				<form>
+					<div className='row mb-5 d-flex flex-column justify-content-center'>
+						<div className='col-12 d-flex justify-content-center align-items-center'>
+							<input
+								className=" w-50 text-center"
+								style={{
+									height:'5rem',
+									fontSize:'20px',
+									backgroundColor: `${passwordStrength.backgroundColor ? passwordStrength.backgroundColor: ''}`
+								}}
+								value={password}
+								type="text"
+								name="passwordChecker"
+								placeholder='Enter Your Password'
+								onChange={handleChange}
+							/>
+						</div>
+
+						<div className='col-12 d-flex justify-content-center align-items-center mt-3'>
+							<div className="pwd-label">
+								{password && (
+									<div>
+										<p
+											className="text-uppercase fw-bold"
+											style={{
+												color: `${passwordStrength.backgroundColor}`
+											}}
+										>
+											{passwordStrength.val}
+										</p>
+									</div>
+								)}
+							</div>
+						</div>
+						<div className='col-12 d-flex text-center justify-content-center align-items-center mt-3'>
+							{crackDuration &&
+								`It would take a computer about ${crackDuration} to crack your password`
+							}
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+  	);
 };
 export default StrengthMeter;
--- src/pages/TestPasswordPage/index.js
+++ src/pages/TestPasswordPage/index.js
@@ -1,45 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './style.css';
 import StrengthMeter from '../../components/StrengthMeter';
 import Label from '../../components/Label';
 
 function TestPasswordPage() {
-	const [password, setPassword] = useState(null);
-
-	const handleChange = (event) => {
-	  // 👇 Get input value from "event"
-	  setPassword(event.target.value);
-	  console.log(password);
-	};
   	return (
 		<div className='w-100 d-flex flex-column justify-content-center '>
 			<div className='d-flex justify-content-center'>
 				<Label text='Check Your Password Strength'></Label>
 			</div>
 			<div className='row'>
-				<form>
-					<div className='row mb-5'>
-						<div className='col-12 d-flex justify-content-center align-items-center'>
-
-								<input
-									className=" w-50 text-center"
-									style={{
-										height:'5rem',
-										fontSize:'20px'
-									}}
-									value={password}
-									type="text"
-									name="passwordChecker"
-									placeholder='Enter Your Password'
-									onChange={handleChange}
-								/>
-						</div>
-						<div className='col-12 d-flex justify-content-center align-items-center'>
-								<StrengthMeter password={password}>
-								</StrengthMeter>
-						</div>
-					</div>
-				</form>
+				<div className='col-12 d-flex justify-content-center align-items-center'>
+					<StrengthMeter></StrengthMeter>
+				</div>
 			</div>
 		</div>
   	)
